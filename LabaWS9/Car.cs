e949251501283692db37ae68a55e6b145417a058
mushroomvoodoo <normeeboy@gmail.com>
2024-05-02 08:27:37 +0300

    ...

---
+++ LabaWS9/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaWS9
+{
+    internal class Car
+    {
+        internal protected string Mark { get; private set; }
+        internal protected string Number { get; private set; }
+        internal int Pollution { get; set; }
+        public Car()
+        {
+            Mark = "Undefined";
+            Number = "ОО000О";
+            Pollution = 100;
+        }
+        public Car(string mark, string number)
+        {
+            Mark = mark;
+            Number = number;
+            Pollution = new Random().Next(0, 101);
+        }
+        public void getInfo()
+        {
+            Console.WriteLine($"Машина {Mark}, с номером {Number}, она загрязнена на {Pollution} %");
+        }
+    }
+    internal class Garage
+    {
+        public void Stay(Car car)
+        {
+            car.Pollution += new Random().Next(0, 2);
+            Console.WriteLine($"Машина {car.Mark} стоит в гараже, она загрязнена на {car.Pollution} %");
+        }
+    }
+    internal class Washer
+    {
+        public void Wash(Car car)
+        {
+            car.Pollution = 0;
+            Console.WriteLine($"Машина {car.Mark} помыта, теперь она загрязнена на {car.Pollution} %");
+        }
+    }
+}
--- LabaWS9/Program.cs
+++ LabaWS9/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using LabaWS9;
+
+internal class Program
+{
+    delegate void CWCleaner(Car car);
+
+    private static void Main(string[] args)
+    {
+        Car audi = new Car("AUDI TT", "КП345Т");
+        Washer CleanWheel = new();
+        Garage myGarage = new();
+        audi.getInfo();
+        myGarage.Stay(audi);
+        Car[] cars = new Car[5];
+        cars[0] = audi;
+        for (int i = cars.Length-1; i >= 1; i--)
+        {
+            cars[i] = new Car();
+        }
+        //CleanWheel.Wash(audi);
+        CWCleaner cleaner = CleanWheel.Wash;
+        for (int i = cars.Length-1; i >= 0; i--)
+        {
+            cleaner(cars[i]);
+        }
+    }
+}
